10ee3270b2ba11331e994dde8861e52deed83e90
Angel  Dimitroff <angeldimitrov05@gmail.com>
2024-12-29 19:46:25 +0200

    solution for day 6.2

--- Day6/day6.2.py
+++ Day6/day6.2.py
@@ -37,9 +37,10 @@ current_guard_symbol = copy.copy(GUARD_SYMBOL)
 has_guard_position_reached = False
 for current_y, current_y_line in enumerate(guard_map):
     for current_x, current_symbol in enumerate(current_y_line):
-        if current_symbol == GUARD_SYMBOL:
+        if current_symbol != OBSTACLE_SYMBOL and current_symbol != FREE_SYMBOL and current_symbol != '\n':
             guard_position = Position(current_x, current_y)
             has_guard_position_reached = True
+            current_guard_symbol = current_symbol
             break
     if has_guard_position_reached:
         break
@@ -82,43 +83,44 @@ used_positions = [ [PositionInformation(Position(x, y), value) for x, value in e
 cached_closest_obstacle_distance = []
 outside_map_position = Position(-2, -2)
 
-for y, line in enumerate(guard_map):
-    cached_closest_obstacle_distance.append([])
-    for x, symbol in enumerate(line):
-        if symbol == '\n':
-            continue
+def cache_obstacles():
+    for y, line in enumerate(guard_map):
+        cached_closest_obstacle_distance.append([])
+        for x, symbol in enumerate(line):
+            if symbol == '\n':
+                continue
 
-        if symbol == OBSTACLE_SYMBOL:
-            nearestObstacles = CachedNearestObstacle(Position(-10, -10), Position(-10, -10), Position(-10, -10), Position(-10, -10))
+            if symbol == OBSTACLE_SYMBOL:
+                nearestObstacles = CachedNearestObstacle(Position(-10, -10), Position(-10, -10), Position(-10, -10), Position(-10, -10))
+                cached_closest_obstacle_distance[y].append(nearestObstacles)
+                continue
+            
+            nearestObstacles = CachedNearestObstacle(Position(-1, y), Position(guard_map_line_size, y), Position(x, -1), Position(x, guard_map_size))
+
+            # calculate the distance for this
+            for adjacent_x in range(guard_map_line_size)[1:]:
+                if x - adjacent_x >= 0 and guard_map[y][x - adjacent_x] == OBSTACLE_SYMBOL and x - adjacent_x + 1 > nearestObstacles.lhs_position.x:
+                    nearestObstacles.lhs_position = Position(x - adjacent_x + 1, y)
+
+                if x + adjacent_x < guard_map_line_size and guard_map[y][x + adjacent_x] == OBSTACLE_SYMBOL and x + adjacent_x - 1 < nearestObstacles.rhs_position.x:
+                    nearestObstacles.rhs_position = Position(x + adjacent_x - 1, y)
+
+            for adjacent_y in range(guard_map_size)[1:]:
+                if y - adjacent_y >= 0 and guard_map[y - adjacent_y][x] == OBSTACLE_SYMBOL and y - adjacent_y + 1 > nearestObstacles.up_position.y:
+                    nearestObstacles.up_position = Position(x, y - adjacent_y + 1)
+
+                if y + adjacent_y < guard_map_size and guard_map[y + adjacent_y][x] == OBSTACLE_SYMBOL and y + adjacent_y - 1 < nearestObstacles.down_position.y:
+                    nearestObstacles.down_position = Position(x, y + adjacent_y - 1)
+
+            if nearestObstacles.lhs_position.x == -1 and nearestObstacles.lhs_position.y == y:
+                nearestObstacles.lhs_position = outside_map_position
+            if nearestObstacles.rhs_position.x == guard_map_line_size and nearestObstacles.rhs_position.y == y:
+                nearestObstacles.rhs_position = outside_map_position
+            if nearestObstacles.up_position.x == x and nearestObstacles.up_position.y == -1:
+                nearestObstacles.up_position = outside_map_position
+            if nearestObstacles.down_position.x == x and nearestObstacles.down_position.y == guard_map_size:
+                nearestObstacles.down_position = outside_map_position
             cached_closest_obstacle_distance[y].append(nearestObstacles)
-            continue
-        
-        nearestObstacles = CachedNearestObstacle(Position(-1, y), Position(guard_map_line_size, y), Position(x, -1), Position(x, guard_map_size))
-
-        # calculate the distance for this
-        for adjacent_x in range(guard_map_line_size):
-            if x - adjacent_x >= 0 and guard_map[y][x - adjacent_x] == OBSTACLE_SYMBOL and x - adjacent_x > nearestObstacles.lhs_position.x:
-                nearestObstacles.lhs_position = Position(x - adjacent_x + 1, y)
-            
-            if x + adjacent_x < guard_map_line_size and guard_map[y][x + adjacent_x] == OBSTACLE_SYMBOL and x + adjacent_x < nearestObstacles.rhs_position.x:
-                nearestObstacles.rhs_position = Position(x + adjacent_x - 1, y)
-        
-        for adjacent_y in range(guard_map_size):
-            if y - adjacent_y >= 0 and guard_map[y - adjacent_y][x] == OBSTACLE_SYMBOL and y - adjacent_y > nearestObstacles.up_position.y:
-                nearestObstacles.up_position = Position(x, y - adjacent_y + 1)
-            
-            if y + adjacent_y < guard_map_size and guard_map[y + adjacent_y][x] == OBSTACLE_SYMBOL and y + adjacent_y < nearestObstacles.down_position.y:
-                nearestObstacles.down_position = Position(x, y + adjacent_y - 1)
-
-        if nearestObstacles.lhs_position.x == -1 and nearestObstacles.lhs_position.y == y:
-            nearestObstacles.lhs_position = outside_map_position
-        if nearestObstacles.rhs_position.x == guard_map_line_size and nearestObstacles.rhs_position.y == y:
-            nearestObstacles.rhs_position = outside_map_position
-        if nearestObstacles.up_position.x == x and nearestObstacles.up_position.y == -1:
-            nearestObstacles.up_position = outside_map_position
-        if nearestObstacles.down_position.x == x and nearestObstacles.down_position.y == guard_map_size:
-            nearestObstacles.down_position = outside_map_position
-        cached_closest_obstacle_distance[y].append(nearestObstacles)
 
 number_of_cycles_found = 0
 
@@ -126,23 +128,24 @@ while not has_guard_reached_border:
 
     next_guard_position = move_guard(current_guard_symbol) + guard_position
     if ((next_guard_position.y == 0 and current_guard_symbol == '^') or \
-        (next_guard_position.y == guard_map_size - 1 and current_guard_symbol == 'v') or \
+        (next_guard_position.y >= guard_map_size - 1 and current_guard_symbol == 'v') or \
         (next_guard_position.x == 0 and current_guard_symbol == '<') or \
-        (next_guard_position.x == guard_map_line_size - 1 and current_guard_symbol == '>')) and \
-            guard_map[next_guard_position.y][next_guard_position.x] != OBSTACLE_SYMBOL:
-         used_positions[next_guard_position.y][next_guard_position.x].current_symbol = current_guard_symbol
-         used_positions[next_guard_position.y][next_guard_position.x].previous_position = guard_position
-         has_guard_reached_border = True
+        (next_guard_position.x >= guard_map_line_size - 1 and current_guard_symbol == '>')) and \
+            guard_map[next_guard_position.y][next_guard_position.x] != OBSTACLE_SYMBOL:  
+            if used_positions[next_guard_position.y][next_guard_position.x].current_symbol == FREE_SYMBOL:
+                used_positions[next_guard_position.y][next_guard_position.x].current_symbol = current_guard_symbol
+                used_positions[next_guard_position.y][next_guard_position.x].previous_position = guard_position
+            has_guard_reached_border = True
         
     if guard_map[next_guard_position.y][next_guard_position.x] == OBSTACLE_SYMBOL:
         current_guard_symbol = rotate_guard_direction(current_guard_symbol)
-    else:
+    elif used_positions[next_guard_position.y][next_guard_position.x].current_symbol == FREE_SYMBOL:
         used_positions[next_guard_position.y][next_guard_position.x].previous_position = guard_position
         used_positions[next_guard_position.y][next_guard_position.x].current_symbol = current_guard_symbol
 
         guard_position = next_guard_position
-
-# number_of_entered_positions = 0
+    else:
+        guard_position = next_guard_position
 
 obstacle_positions = []
 
@@ -164,8 +167,6 @@ def find_next_obstacle(position, direction):
     
     print("find_next_obstacle() -> That shouldn't have happened")
     return Position(-2, -2)
-        
-        
 
 def try_creating_cycle(cycle_position, guard_position, current_guard_symbol):
     guard_map_size = len(guard_map)
@@ -174,7 +175,7 @@ def try_creating_cycle(cycle_position, guard_position, current_guard_symbol):
     has_guard_position_reached = False
     has_entered_cycle = False
 
-    used_map_directions = [ [x for x in line ] for line in guard_map]
+    used_map_directions = [ [0 for _ in line ] for line in guard_map]
 
     while not has_guard_position_reached and not has_entered_cycle:
         next_position = find_next_obstacle(guard_position, current_guard_symbol)
@@ -185,14 +186,15 @@ def try_creating_cycle(cycle_position, guard_position, current_guard_symbol):
         current_guard_symbol = rotate_guard_direction(current_guard_symbol)
         guard_position = next_position
 
-        if used_map_directions[guard_position.y][guard_position.x] == current_guard_symbol:
+        if used_map_directions[guard_position.y][guard_position.x] > 4:
             has_entered_cycle = True
 
-        used_map_directions[guard_position.y][guard_position.x] = current_guard_symbol
+        used_map_directions[guard_position.y][guard_position.x] = used_map_directions[guard_position.y][guard_position.x] + 1
 
     return has_entered_cycle
 
 number_cycles_found = 0
+number_of_new_paths = 0
 
 def update_cached_obstacles(obstacle_position):
     for adjacent_x in range(guard_map_line_size)[1:]:
@@ -223,10 +225,12 @@ def update_cached_obstacles(obstacle_position):
 
     cached_closest_obstacle_distance[obstacle_position.y][obstacle_position.x] = CachedNearestObstacle(Position(-10, -10), Position(-10, -10), Position(-10, -10), Position(-10, -10))
 
+cache_obstacles()
+
 for current_y, line in enumerate(used_positions):
     for current_x, has_guard_been_there_initially in enumerate(line):
         if current_y == initial_guard_position.y and current_x == initial_guard_position.x:
-            break
+            continue
 
         if has_guard_been_there_initially.current_symbol != OBSTACLE_SYMBOL and has_guard_been_there_initially.current_symbol != FREE_SYMBOL and \
             has_guard_been_there_initially.current_symbol != '\n' :
@@ -248,20 +252,27 @@ for current_y, line in enumerate(used_positions):
 
                 update_cached_obstacles(Position(current_x, current_y))
 
-                current_guard_position = copy.deepcopy(initial_guard_position)
-                current_guard_symbol = '^'
+                current_guard_position = used_positions[current_y][current_x].previous_position
+                current_guard_symbol = used_positions[current_guard_position.y][current_guard_position.x].current_symbol
+
                 has_found_cycle_result = try_creating_cycle(Position(current_x, current_y), current_guard_position, current_guard_symbol)
                 number_cycles_found = number_cycles_found + has_found_cycle_result
+                number_of_new_paths = number_of_new_paths + 1
+
                 for horizontal in range(guard_map_line_size):
                     cached_closest_obstacle_distance[current_y][horizontal].lhs_position = changed_horizontal_line[horizontal].lhs_position
                     cached_closest_obstacle_distance[current_y][horizontal].rhs_position = changed_horizontal_line[horizontal].rhs_position
+                    cached_closest_obstacle_distance[current_y][horizontal].up_position = changed_horizontal_line[horizontal].up_position
+                    cached_closest_obstacle_distance[current_y][horizontal].down_position = changed_horizontal_line[horizontal].down_position
                 
                 for vertical in range(guard_map_size):
                     cached_closest_obstacle_distance[vertical][current_x].up_position = changed_vertical_line[vertical].up_position
                     cached_closest_obstacle_distance[vertical][current_x].down_position = changed_vertical_line[vertical].down_position
+                    cached_closest_obstacle_distance[vertical][current_x].lhs_position = changed_vertical_line[vertical].lhs_position
+                    cached_closest_obstacle_distance[vertical][current_x].rhs_position = changed_vertical_line[vertical].rhs_position
 
                 if has_found_cycle_result:
-                    print("Cycle found at position: ", current_x, current_y)
+                    print(f"Found cycle at {current_x}, {current_y}")
                 break
 
 print(number_cycles_found)
