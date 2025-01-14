ceb898e989e94ef96ee59c2c4de3b5a47c75a4a6
Angel  Dimitroff <angeldimitrov05@gmail.com>
2025-01-15 01:54:57 +0200

    added solution for day11.2

--- Day11/day11.2.py
+++ Day11/day11.2.py
@@ -12,10 +12,13 @@ class Stone:
     def __repr__(self):
         return f'Number: {self.number}, Generation: {self.generation}'
 
+NOT_ITERATED_STONE_GENERATION_INDEX = 0
+
 class StoneCache:
-    def __init__(self, lhs_list, rhs_list):
+    def __init__(self, lhs_list, rhs_list, number_of_max_generations):
         self.lhs_list = lhs_list
         self.rhs_list = rhs_list
+        self.cache_stones_result = [NOT_ITERATED_STONE_GENERATION_INDEX for _ in range(number_of_max_generations)]
 
 input_file = open('./day11/input.txt', 'r', encoding='utf-8')
 
@@ -65,55 +68,101 @@ STONE_MULTIPLIER = 2024
 
 cached_result_after_iterations = {}
 
-for stone in stone_numbers:
-    initial_stone_number = stone.number
+def mutate_stone(stone):
     if stone.generation == NUMBER_OF_ITERATIONS:
-        continue
-
-    if stone.number in cached_result_after_iterations:
-        # you have already iterated those generation, add the rhs stones and change the value to the generation that is remaining
-        size_of_cached_result = len(cached_result_after_iterations[stone.number].lhs_list)
-
-        # everything is already calculated, just get it
-        if size_of_cached_result >= NUMBER_OF_ITERATIONS - stone.generation:
-            lhs_index_taken = NUMBER_OF_ITERATIONS - stone.generation - 1
-            for rhs_stone in cached_result_after_iterations[stone.number].rhs_list:
-                if rhs_stone.generation > lhs_index_taken + 1:
-                    break
-
-                stone_numbers.append(Stone(rhs_stone.number, rhs_stone.generation + stone.generation))
-
-            stone.number = cached_result_after_iterations[stone.number].lhs_list[lhs_index_taken]
-            stone.generation = NUMBER_OF_ITERATIONS
-
-        else:
-            for rhs_stone in cached_result_after_iterations[stone.number].rhs_list:
-                stone_numbers.append(Stone(rhs_stone.number, rhs_stone.generation + stone.generation))
-
-            stone.number = cached_result_after_iterations[stone.number].lhs_list[size_of_cached_result - 1]
-            stone.generation += size_of_cached_result
-
-    else:
-        cached_result_after_iterations[stone.number] = StoneCache([], [])
+        return 1
     
-    for current_generation in range(NUMBER_OF_ITERATIONS - stone.generation):
-        if stone.number == 0:
-            stone.number = 1
-            cached_result_after_iterations[initial_stone_number].lhs_list.append(1)
-            continue
-
-        lhs, rhs = try_splitting_number(stone.number)
-        if rhs is None:
-            stone.number *= STONE_MULTIPLIER
-            cached_result_after_iterations[initial_stone_number].lhs_list.append(stone.number)
-        else:
-            stone.number = lhs
-            stone_numbers.append(Stone(rhs, stone.generation + current_generation + 1))
-            cached_result_after_iterations[initial_stone_number].lhs_list.append(lhs)
-            len_foo = len(cached_result_after_iterations[initial_stone_number].lhs_list)
-            cached_result_after_iterations[initial_stone_number].rhs_list.append(Stone(rhs, len_foo))
+    if cached_result_after_iterations[stone.number].cache_stones_result[stone.generation] != NOT_ITERATED_STONE_GENERATION_INDEX:
+        return cached_result_after_iterations[stone.number].cache_stones_result[stone.generation]
     
-    stone.generation = NUMBER_OF_ITERATIONS
+    if stone.number == 0:
+        if 1 not in cached_result_after_iterations:
+            cached_result_after_iterations[1] = StoneCache([], [], NUMBER_OF_ITERATIONS) 
+
+        stone_children = mutate_stone(Stone(1, stone.generation + 1))
+        cached_result_after_iterations[stone.number].cache_stones_result[stone.generation] = stone_children
+        return stone_children
+    
+    lhs, rhs = try_splitting_number(stone.number)
+    if rhs is None:
+        child_value = stone.number * STONE_MULTIPLIER
+        if child_value not in cached_result_after_iterations:
+            cached_result_after_iterations[child_value] = StoneCache([], [], NUMBER_OF_ITERATIONS)
+
+        stone_children = mutate_stone(Stone(child_value, stone.generation + 1))
+        cached_result_after_iterations[stone.number].cache_stones_result[stone.generation] = stone_children 
+        return stone_children
+        
+    if lhs not in cached_result_after_iterations:
+        cached_result_after_iterations[lhs] = StoneCache([], [], NUMBER_OF_ITERATIONS)
+    if rhs not in cached_result_after_iterations:
+        cached_result_after_iterations[rhs] = StoneCache([], [], NUMBER_OF_ITERATIONS)
+        
+    lhs_stone_children = mutate_stone(Stone(lhs, stone.generation + 1))
+    rhs_stone_children = mutate_stone(Stone(rhs, stone.generation + 1))
+
+    cached_result_after_iterations[stone.number].cache_stones_result[stone.generation] = lhs_stone_children + rhs_stone_children
+    return lhs_stone_children + rhs_stone_children
+
+result = 0
+for current_stone in stone_numbers:
+    if current_stone.number not in cached_result_after_iterations:
+        cached_result_after_iterations[current_stone.number] = StoneCache([], [], NUMBER_OF_ITERATIONS)
+
+    result += mutate_stone(current_stone)
+
+print(result)
+
+
+# for stone in stone_numbers:
+#     initial_stone_number = stone.number
+#     if stone.generation == NUMBER_OF_ITERATIONS:
+#         continue
+
+#     if stone.number in cached_result_after_iterations:
+#         # you have already iterated those generation, add the rhs stones and change the value to the generation that is remaining
+#         size_of_cached_result = len(cached_result_after_iterations[stone.number].lhs_list)
+
+#         # everything is already calculated, just get it
+#         if size_of_cached_result >= NUMBER_OF_ITERATIONS - stone.generation:
+#             lhs_index_taken = NUMBER_OF_ITERATIONS - stone.generation - 1
+#             for rhs_stone in cached_result_after_iterations[stone.number].rhs_list:
+#                 if rhs_stone.generation > lhs_index_taken + 1:
+#                     break
+
+#                 stone_numbers.append(Stone(rhs_stone.number, rhs_stone.generation + stone.generation))
+
+#             stone.number = cached_result_after_iterations[stone.number].lhs_list[lhs_index_taken]
+#             stone.generation = NUMBER_OF_ITERATIONS
+
+#         else:
+#             for rhs_stone in cached_result_after_iterations[stone.number].rhs_list:
+#                 stone_numbers.append(Stone(rhs_stone.number, rhs_stone.generation + stone.generation))
+
+#             stone.number = cached_result_after_iterations[stone.number].lhs_list[size_of_cached_result - 1]
+#             stone.generation += size_of_cached_result
+
+#     else:
+#         cached_result_after_iterations[stone.number] = StoneCache([], [])
+    
+#     for current_generation in range(NUMBER_OF_ITERATIONS - stone.generation):
+#         if stone.number == 0:
+#             stone.number = 1
+#             cached_result_after_iterations[initial_stone_number].lhs_list.append(1)
+#             continue
+
+#         lhs, rhs = try_splitting_number(stone.number)
+#         if rhs is None:
+#             stone.number *= STONE_MULTIPLIER
+#             cached_result_after_iterations[initial_stone_number].lhs_list.append(stone.number)
+#         else:
+#             stone.number = lhs
+#             stone_numbers.append(Stone(rhs, stone.generation + current_generation + 1))
+#             cached_result_after_iterations[initial_stone_number].lhs_list.append(lhs)
+#             len_foo = len(cached_result_after_iterations[initial_stone_number].lhs_list)
+#             cached_result_after_iterations[initial_stone_number].rhs_list.append(Stone(rhs, len_foo))
+    
+#     stone.generation = NUMBER_OF_ITERATIONS
 
 # print(stone_numbers)
-print(len(stone_numbers))
+# print(len(stone_numbers))
